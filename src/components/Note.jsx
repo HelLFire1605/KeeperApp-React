47020041de34e13bc4de98a8d2dd3d73f2e28b4a
Prathamesh Ananda Shingan <119754494+HelLFire1605@users.noreply.github.com>
2023-07-21 17:44:19 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import DeleteOutlineRoundedIcon from "@mui/icons-material/DeleteOutlineRounded";
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>
+        <DeleteOutlineRoundedIcon />
+      </button>
+    </div>
+  );
+}
+
+export default Note;
